yingxin-gh/k0s

commit e1e9ca5e7a570bd9bdb6fb72956b42a258335fdf

[Konnectivity] More restrictive security context
Signed-off-by: Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>

pkg/component/controller/konnectivityagent.go
@@ -230,6 +230,12 @@ spec:
         prometheus.io/port: '8093'
     spec:
       securityContext:
+        allowPrivilegeEscalation: false
+        capabilities:
+          drop:
+          - all
+        readOnlyRootFilesystem: true
+        runAsNonRoot: true
         supplementalGroups: [0]` /* in order to read the projected service account token */ + `
       nodeSelector:
         kubernetes.io/os: linux

commit f5448c47805ba35905d314472609e5fc4374192c

Fix the security context of konnectivity-agent
The allowPrivilegeEscalation, capabilities and readOnlyRootFilesystem
fields do not belong to the pod security context, but to the container's
security context instead.

Fixes: e1e9ca5e7 ("[Konnectivity] More restrictive security context")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/konnectivityagent.go
@@ -235,11 +235,6 @@ spec:
         prometheus.io/port: '8093'
     spec:
       securityContext:
-        allowPrivilegeEscalation: false
-        capabilities:
-          drop:
-          - all
-        readOnlyRootFilesystem: true
         runAsNonRoot: true
         supplementalGroups: [0]` /* in order to read the projected service account token */ + `
       nodeSelector:
@@ -273,6 +268,12 @@ spec:
             - --service-account-token-path=/var/run/secrets/tokens/konnectivity-agent-token
             - --agent-identifiers=host=$(NODE_IP)
             - --agent-id=$(NODE_IP)
+          securityContext:
+            allowPrivilegeEscalation: false
+            capabilities:
+              drop:
+              - all
+            readOnlyRootFilesystem: true
           volumeMounts:
             - mountPath: /var/run/secrets/tokens
               name: konnectivity-agent-token