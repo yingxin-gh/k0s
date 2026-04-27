yingxin-gh/k0s

commit cafbc16e5670b3768e7bc49ae0656cc54e03c053

Add SPDX/REUSE copyright/license headers to Markdown docs
Add support for them to hack/copyright.sh, as well.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

hack/copyright.sh
@@ -30,9 +30,14 @@ has_date_copyright(){
 	grep -q -F "SPDX-FileCopyrightText: $DATE k0s authors" "$FILE"
 }
 
-# Deliberately do not search in docs as the date of the matches for the
-# Copyright notice aren't related to the date of the document.
-for i in $(find cmd hack internal inttest pkg static -type f -name '*.go' -not -name 'zz_generated*'); do
+find_files_to_check() {
+    # All the Go files
+    find cmd hack internal inttest pkg static -type f -name '*.go' -not -name 'zz_generated*'
+    # All the markdown documentation, excluding the auto-generated CLI docs
+    find docs -type f -name '*.md' -not -path 'docs/cli/*'
+}
+
+for i in $(find_files_to_check); do
     case "$i" in
     pkg/client/clientset/*)
         if ! has_basic_copyright "$i"; then

commit 3e04d17d40d1adad4900991f6a9c9749974d9020

Lint doc copyright in docs CI workflow
Previously, the docs were only linted in the standard CI workflow.
However, that one is not triggered for docs-only PRs. This means that
CI on a PR could pass, but subsequent CI runs on the main branch would
fail.

As the docs workflow is triggered whenever a Markdown file is touched,
split up the copyright notice check into checks for code-related files
and docs-related files. Add missing SPDX headers to the Markdown files
that didn't have them already.

Fixes: cafbc16e5 ("Add SPDX/REUSE copyright/license headers to Markdown docs")
Fixes: 3f8668c95 ("docs: document version skew policy")
Fixes: a59591cff ("docs: document k0s data and run directories")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

.github/workflows/docs.yml
@@ -23,7 +23,14 @@ jobs:
     runs-on: ubuntu-24.04
     steps:
       - uses: actions/checkout@34e114876b0b11c390a56381ad16ebd13914f8d5 # v4.3.1
+        with:
+          fetch-depth: 0 # for copyright linting
+          persist-credentials: false
+
       - uses: DavidAnson/markdownlint-cli2-action@ce4853d43830c74c1753b39f3cf40f71c2031eb9 # v23.0.0
         with:
           config: .markdownlint.jsonc
           globs: '**/*.md'
+
+      - name: Check copyright headers
+        run: WHAT=docs make lint-copyright

.github/workflows/lint.yaml
@@ -128,7 +128,7 @@ jobs:
         run: make codegen && git diff --exit-code
 
       - name: Check copyright headers
-        run: make lint-copyright
+        run: WHAT=code make lint-copyright
 
   validate-os-tests:
     name: Validate OS tests

hack/copyright.sh
@@ -7,6 +7,7 @@ set -eu
 
 RESULT=0
 FIX=${FIX:=n}
+WHAT=${WHAT:=all}
 
 get_year(){
     # The -1 has to be added because if a file has been added, removed and added
@@ -31,10 +32,19 @@ has_date_copyright(){
 }
 
 find_files_to_check() {
+    case "$WHAT" in
     # All the Go files
-    find cmd hack internal inttest pkg static -type f -name '*.go' -not -name 'zz_generated*'
+    all | code)
+        find cmd hack internal inttest pkg static -type f -name '*.go' -not -name 'zz_generated*'
+        ;;
+    esac
+
+    case "$WHAT" in
     # All the markdown documentation, excluding the auto-generated CLI docs
-    find docs -type f -name '*.md' -not -path 'docs/cli/*'
+    all | docs)
+        git ls-files '*.md'
+        ;;
+    esac
 }
 
 for i in $(find_files_to_check); do